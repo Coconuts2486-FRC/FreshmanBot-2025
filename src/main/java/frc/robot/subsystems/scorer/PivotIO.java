3c750d8b0122b3883a89e2a3abcdb69af4633f72
pinkfqx <noahclark2020@icloud.com>
2025-10-09 21:11:30 -0700

    Broken pid stuf whoopee

--- src/main/java/frc/robot/subsystems/scorer/PivotIO.java
+++ src/main/java/frc/robot/subsystems/scorer/PivotIO.java
@@ -5,6 +5,8 @@ import frc.robot.util.RBSIIO;
 public interface PivotIO extends RBSIIO {
   public default void setVelocity(double velocityInput) {}
 
+  public default void configPID(double kP, double kI, double kD) {}
+
   public default double groundPivotPose() {
     return 0.0;
   }
--- src/main/java/frc/robot/subsystems/scorer/PivotIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/scorer/PivotIOTalonFX.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems.scorer;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class PivotIOTalonFX implements PivotIO {
   // Define Motor/Encoder and set ids/channels
   private final TalonFX Pivot = new TalonFX(31);
   private final DutyCycleEncoder groundPivotEncoder = new DutyCycleEncoder(1);
+  private final PIDController pid = new PIDController(4, 0, .5);
 
   // Motor Commands
   @Override
@@ -23,5 +27,17 @@ public class PivotIOTalonFX implements PivotIO {
   @Override
   public double groundPivotPose() {
     return groundPivotEncoder.get();
+  }
+
+  // PID
+
+  @Override
+  public void configPID(double kP, double kI, double kD) {
+    Slot0Configs pid = new Slot0Configs();
+    pid.withKP(kP);
+    pid.withKI(kI);
+    pid.withKD(kD);
+    
+    .getConfigurator().apply(pid);
   }
 }
